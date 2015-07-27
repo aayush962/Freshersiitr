bd49bdfb9e366d621d247d9982d40dae4a2d94ce
Aayush Agarwal <aayush962@gmail.com>
2015-07-28 00:16:04 +0530

    added validations

--- app/models/fresher.rb
+++ app/models/fresher.rb
@@ -1,3 +1,7 @@
 class Fresher < ActiveRecord::Base
-	validates :name, :email, :branch, :contact, :skills,  presence: true
+	validates :name, :email, :branch, :contact,  presence: true
+	validates_length_of :contact, :is => 10, :message => "Don't Play! Enter a correct mobile number."
+	validates :contact, numericality: true
+	validates_length_of :enroll, :is => 8, :message => "Get your ID!! Enrollment number is 8 digits"
+	validates :skills, presence: true
 end
